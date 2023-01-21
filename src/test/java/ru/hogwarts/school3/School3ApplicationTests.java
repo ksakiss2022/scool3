d247e1265c3519dffe2137339b61e7c38de0bc5a
Swetlana <sweta_zaika@mail.ru>
2023-01-21 18:41:31 +0300

    scool3

---
+++ src/test/java/ru/hogwarts/school3/School3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class School3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
